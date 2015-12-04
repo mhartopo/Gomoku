3a22753210a1f8d63d06e884f9b71c33ec59fe06
fikriaulia <fikri.aulia13@gmail.com>
2015-12-04 15:20:18 +0700

    connected server with multiple client

--- src/network/Client.java
+++ src/network/Client.java
@@ -1,65 +1,81 @@
 package network;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 public class Client {
-    public static void main(String[] args) throws IOException {
 
-        String serverHostname = new String ("127.0.0.1");
+    BufferedReader in;
+    PrintWriter out;
+    JFrame frame = new JFrame("Chatter");
+    JTextField textField = new JTextField(40);
+    JTextArea messageArea = new JTextArea(8, 40);
+    public Client() {
 
-        if (args.length > 0)
-           serverHostname = args[0];
-        System.out.println ("Attemping to connect to host " +
-                serverHostname + " on port 10008.");
+        // Layout GUI
+        textField.setEditable(false);
+        messageArea.setEditable(false);
+        frame.getContentPane().add(textField, "North");
+        frame.getContentPane().add(new JScrollPane(messageArea), "Center");
+        frame.pack();
 
-        Socket echoSocket = null;
-        PrintWriter out = null;
-        BufferedReader in = null;
+        // Add Listeners
+        textField.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                out.println(textField.getText());
+                textField.setText("");
+            }
+        });
+    }
+    private String getServerAddress() {
+        return JOptionPane.showInputDialog(
+            frame,
+            "Enter IP Address of the Server:",
+            "Welcome to the Chatter",
+            JOptionPane.QUESTION_MESSAGE);
+    }
+    private String getName() {
+        return JOptionPane.showInputDialog(
+            frame,
+            "Choose a screen name:",
+            "Screen name selection",
+            JOptionPane.PLAIN_MESSAGE);
+    }
+    private void run() throws IOException {
 
-        try {
-            echoSocket = new Socket(serverHostname, 10008);
-            out = new PrintWriter(echoSocket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(
-                                        echoSocket.getInputStream()));
-        } catch (UnknownHostException e) {
-            System.err.println("Don't know about host: " + serverHostname);
-            System.exit(1);
-        } catch (IOException e) {
-            System.err.println("Couldn't get I/O for "
-                               + "the connection to: " + serverHostname);
-            System.exit(1);
+        // Make connection and initialize streams
+        String serverAddress = getServerAddress();
+        Socket socket = new Socket(serverAddress, 9001);
+        in = new BufferedReader(new InputStreamReader(
+            socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream(), true);
+
+        // Process all messages from server, according to the protocol.
+        while (true) {
+            String line = in.readLine();
+            if (line.startsWith("SUBMITNAME")) {
+                out.println(getName());
+            } else if (line.startsWith("NAMEACCEPTED")) {
+                textField.setEditable(true);
+            } else if (line.startsWith("MESSAGE")) {
+                messageArea.append(line.substring(8) + "\n");
+            }
         }
-
-	BufferedReader stdIn = new BufferedReader(
-                                   new InputStreamReader(System.in));
-	BufferedReader msgIn = new BufferedReader(
-			new InputStreamReader( echoSocket.getInputStream())
-			);
-	String userInput, msgInput = null;
-
-        System.out.println ("Type Message (\"Bye.\" to quit)");
-	while (((userInput = stdIn.readLine()) != null) || ((msgInput = msgIn.readLine()) != null)) 
-           {
-	    out.println(userInput);
-
-            // end loop
-	    if (msgInput != null){
-	    	System.out.println("Server : "+msgInput);
-	    }
-	    if (userInput.equals("Bye."))
-                break;
-
-	    System.out.println("echo: " + in.readLine());
-	   }
-
-	out.close();
-	in.close();
-	stdIn.close();
-	echoSocket.close();
+    }
+    public static void main(String[] args) throws Exception {
+        Client client = new Client();
+        client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        client.frame.setVisible(true);
+        client.run();
     }
 }
--- src/network/Server.java
+++ src/network/Server.java
@@ -6,84 +6,75 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-
-public class Server extends Thread
-{ 
-protected Socket clientSocket;
-
-public static void main(String[] args) throws IOException 
-   { 
-    ServerSocket serverSocket = null; 
-
-    try { 
-         serverSocket = new ServerSocket(10008); 
-         System.out.println ("Connection Socket Created");
-         try { 
-              while (true)
-                 {
-                  System.out.println ("Waiting for Connection");
-                  new Server (serverSocket.accept()); 
-                 }
-             } 
-         catch (IOException e) 
-             { 
-              System.err.println("Accept failed."); 
-              System.exit(1); 
-             } 
-        } 
-    catch (IOException e) 
-        { 
-         System.err.println("Could not listen on port: 10008."); 
-         System.exit(1); 
-        } 
-    finally
-        {
-         try {
-              serverSocket.close(); 
-             }
-         catch (IOException e)
-             { 
-              System.err.println("Could not close port: 10008."); 
-              System.exit(1); 
-             } 
+import java.util.HashSet;
+public class Server {
+    private static final int PORT = 9001;
+    private static HashSet<String> names = new HashSet<String>();
+    private static HashSet<PrintWriter> writers = new HashSet<PrintWriter>();
+    public static void main(String[] args) throws Exception {
+        System.out.println("The chat server is running.");
+        ServerSocket listener = new ServerSocket(PORT);
+        try {
+            while (true) {
+                new Handler(listener.accept()).start();
+            }
+        } finally {
+            listener.close();
         }
-   }
-
- private Server (Socket clientSoc)
-   {
-    clientSocket = clientSoc;
-    start();
-   }
- 
- public void run()
-   {
-    System.out.println ("New Communication Thread Started");
-
-    try { 
-         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), 
-                                      true); 
-         BufferedReader in = new BufferedReader( 
-                 new InputStreamReader( clientSocket.getInputStream())); 
-
-         String inputLine; 
-
-         while ((inputLine = in.readLine()) != null) 
-             { 
-              System.out.println ("Client: " + inputLine); 
-              out.println("yes"); 
-
-              if (inputLine.equals("Bye.")) 
-                  break; 
-             } 
-
-         out.close(); 
-         in.close(); 
-         clientSocket.close(); 
-        } 
-    catch (IOException e) 
-        { 
-         System.err.println("Problem with Communication Server");
-         System.exit(1); 
-        } 
     }
-} 
+    private static class Handler extends Thread {
+        private String name;
+        private Socket socket;
+        private BufferedReader in;
+        private PrintWriter out;
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+        public void run() {
+            try {
+
+                // Create character streams for the socket.
+                in = new BufferedReader(new InputStreamReader(
+                    socket.getInputStream()));
+                out = new PrintWriter(socket.getOutputStream(), true);
+                while (true) {
+                    out.println("SUBMITNAME");
+                    name = in.readLine();
+                    if (name == null) {
+                        return;
+                    }
+                    synchronized (names) {
+                        if (!names.contains(name)) {
+                            names.add(name);
+                            break;
+                        }
+                    }
+                }
+                out.println("NAMEACCEPTED");
+                writers.add(out);
+                while (true) {
+                    String input = in.readLine();
+                    if (input == null) {
+                        return;
+                    }
+                    for (PrintWriter writer : writers) {
+                        writer.println("MESSAGE " + name + ": " + input);
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println(e);
+            } finally {
+                if (name != null) {
+                    names.remove(name);
+                }
+                if (out != null) {
+                    writers.remove(out);
+                }
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+    }
+}
